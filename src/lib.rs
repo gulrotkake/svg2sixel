75a9a968b85f27f2a0a6df5763a06985d24fedf1
Michael Mortensen <michael@f3k.org>
2024-06-23 15:34:19 +0200

    Map errors instead of unwrapping them in library function

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,34 @@
+use core::fmt;
+
 use resvg::tiny_skia::{self, Pixmap};
 use resvg::usvg::{Options, Tree};
 use sixel_bytes::SixelError;
 
-pub fn svg2sixel(svg: &str) -> Result<String, SixelError> {
+#[derive(Debug)]
+pub enum Svg2SixelError {
+    Usvg(usvg::Error),
+    Sixel(SixelError),
+    BadSize,
+}
+
+impl fmt::Display for Svg2SixelError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Svg2SixelError::Usvg(err) => write!(f, "usvg error: {0}", err),
+            Svg2SixelError::Sixel(err) => write!(f, "sixel error: {0}", err),
+            Svg2SixelError::BadSize => write!(f, "Bad svg pixmap size"),
+        }
+    }
+}
+
+pub fn svg2sixel(svg: &str) -> Result<String, Svg2SixelError> {
     let mut options = Options::default();
     options.fontdb_mut().load_system_fonts();
 
-    let rtree = Tree::from_str(svg, &options).unwrap();
+    let rtree = Tree::from_str(svg, &options).map_err(|err| Svg2SixelError::Usvg(err))?;
     let pixmap_size = rtree.size();
-    let mut pixmap = Pixmap::new(pixmap_size.width() as u32, pixmap_size.height() as u32).unwrap();
+    let mut pixmap = Pixmap::new(pixmap_size.width() as u32, pixmap_size.height() as u32)
+        .ok_or(Svg2SixelError::BadSize)?;
 
     resvg::render(
         &rtree,
@@ -23,4 +43,5 @@ pub fn svg2sixel(svg: &str) -> Result<String, SixelError> {
         sixel_bytes::PixelFormat::RGBA8888,
         sixel_bytes::DiffusionMethod::Stucki,
     )
+    .map_err(|err| Svg2SixelError::Sixel(err))
 }
